d5616da0ea8988226aceedd72c4facf05e26796c
CHAN-CHENG <ccc908925@gmail.com>
2023-12-30 23:15:20 +0800

    style: prettier .eslintrc.cjs
    single quote to double quote

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,32 +2,32 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'semi': ['error', 'always'],
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
+    semi: ["error", "always"],
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
       {
-        'multiline': { 
-          'delimiter': 'semi',
-          'requireLast': true,
+        multiline: {
+          delimiter: "semi",
+          requireLast: true,
         },
-        'singleline': {
-          'delimiter': 'semi',
-          'requireLast': false,
+        singleline: {
+          delimiter: "semi",
+          requireLast: false,
         },
-      }
+      },
     ],
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
