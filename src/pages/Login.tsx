476abe24e86b53e79525545c24e6a15e2313015d
CHAN-CHENG <ccc908925@gmail.com>
2024-01-02 15:01:17 +0800

    feat: 對欄位驗證 & 加上提示錯誤訊息

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,54 @@
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInputs {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInputs>({ mode: "onTouched" });
+
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="mx-auto pt-xxl-10 login_signUp_form_wrap">
       <div className="d-flex flex-column">
         <p className="mb-2 text-primary">享樂酒店，誠摯歡迎</p>
         <h1 className="mb-7">立即開始旅程</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
               電子信箱
             </label>
             <input
               type="email"
-              className="form-control"
+              className={`form-control ${
+                errors.email && "is-invalid border-3"
+              }`}
               id="email"
               placeholder="hello@example.com"
               autoComplete="username"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "電子信箱 必填",
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g,
+                  message: "電子信箱 格式有誤",
+                },
+              })}
             />
+            {errors.email && (
+              <div className="invalid-feedback">{errors.email.message}</div>
+            )}
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">
@@ -25,10 +56,21 @@ const Login = () => {
             </label>
             <input
               type="password"
-              className="form-control"
+              className={`form-control ${
+                errors.password && "is-invalid border-3"
+              }`}
               id="password"
               autoComplete="current-password"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "密碼 必填",
+                },
+              })}
             />
+            {errors.password && (
+              <div className="invalid-feedback">{errors.password.message}</div>
+            )}
           </div>
           <div className="mb-7">
             <div className="d-flex justify-content-between align-items-center">
@@ -49,7 +91,7 @@ const Login = () => {
           </div>
           <div className="mb-7">
             <button
-              type="button"
+              type="submit"
               className="py-3 btn btn-light btn-lg w-100 fw-bold"
             >
               會員登入
