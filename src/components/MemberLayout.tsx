a6a698454aa93f4f1bf049e1d05c8e477b84b07a
jcsamoyed <aruby5223@gmail.com>
2024-01-14 17:23:02 +0800

    feat(Member): 會員名稱使用 Redux 資料

--- src/components/MemberLayout.tsx
+++ src/components/MemberLayout.tsx
@@ -3,8 +3,18 @@ import { NavLink, Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import styles from "../assets/scss/modules/member.module.scss";
+import { useAppSelector } from "../store/hook";
+import { selectUser } from "../store/slices/userSlice";
 
 const MemberLayout = () => {
+  // Banner 資料
+  const user = useAppSelector(selectUser);
+
+  const localStorageName =
+    localStorage.getItem("enjoyment_luxury_hotel_name") || "";
+
+  const userName = user.name || localStorageName;
+
   // 選單資料
   const [tabList] = useState([
     {
@@ -31,7 +41,7 @@ const MemberLayout = () => {
               className="me-5"
               style={{ width: "12%", maxWidth: "144px", minWidth: "72px" }}
             />
-            <h1 className="fw-bold mb-0">Hello, </h1>
+            <h1 className="fw-bold mb-0">Hello, {userName}</h1>
           </div>
         </div>
       </header>
