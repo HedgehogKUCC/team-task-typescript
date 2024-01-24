018252dd32a2a68ccb3586e6b60cb7adb3e73bda
jcsamoyed <aruby5223@gmail.com>
2024-01-24 16:34:44 +0800

    feat(MemberOrder): 歷史訂單 UI

--- src/pages/MemberOrder.tsx
+++ src/pages/MemberOrder.tsx
@@ -203,7 +203,46 @@ const MemberOrder = () => {
             className="bg-white p-5 p-sm-6 p-md-7"
             style={{ borderRadius: "20px" }}
           >
-            <h5 className="text-black fw-bold">歷史訂單</h5>
+            <h5 className="text-black fw-bold mb-5 mb-sm-7">歷史訂單</h5>
+            <div className="d-flex flex-column flex-xl-row border-bottom pb-5 pb-sm-7">
+              <picture
+                className="overflow-hidden flex-shrink-0 mb-5"
+                style={{ width: "120px", height: "80px", borderRadius: "8px" }}
+              >
+                <img
+                  src="https://github.com/hexschool/2022-web-layout-training/blob/main/typescript-hotel/%E6%A1%8C%E6%A9%9F%E7%89%88/room1.png?raw=true"
+                  className="img-fluid"
+                  alt="尊爵雙人房"
+                />
+              </picture>
+              <div className="ms-0 ms-xl-5">
+                <div className="fs-7 text-gray-dark">
+                  預訂參考編號： HH2302183151222
+                </div>
+                <h6 className="text-gray-dark fw-bold my-3">尊爵雙人房</h6>
+                <div className="mb-3">
+                  <div className="text-gray-dark fs-7 mb-2">
+                    住宿天數： 1 晚
+                  </div>
+                  <div className="text-gray-dark fs-7">住宿人數：2 位</div>
+                </div>
+                <div className="mb-3">
+                  <div
+                    className={`text-gray-dark fs-7 mb-2 ${styles.deco_title}`}
+                  >
+                    入住：6 月 10 日星期二
+                  </div>
+                  <div
+                    className={`text-gray-dark fs-7 ${styles.deco_title_green}`}
+                  >
+                    退房：6 月 11 日星期三
+                  </div>
+                </div>
+                <div className="text-gray-dark fs-7 fw-bold">NT$10,000</div>
+              </div>
+            </div>
+
+            <Button text="查看更多" btnType="secondary" fit="container" />
           </div>
         </div>
       </div>
