d9d493fcdd3b3fb3be7264e05584a459ad1ad259
CHAN-CHENG <ccc908925@gmail.com>
2024-02-02 16:12:15 +0800

    feat: (房型詳細頁) 日期套件 & use Store

--- src/pages/RoomDetail.tsx
+++ src/pages/RoomDetail.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { apiGetRoom } from "../api/apiRoom";
 import type { Room } from "../api/interface/room";
 import Button from "../components/Button";
@@ -9,16 +9,30 @@ import SizeIcon from "/ic_Size.svg";
 import PersonIcon from "/ic_Person.svg";
 import CarIcon from "/ic_Car.svg";
 import CheckIcon from "/ic_check_primary.svg";
-import Calendar from '../components/Calendar';
 
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRange } from "react-date-range";
+import { zhTW } from "date-fns/locale";
+
+import { useAppDispatch } from "../store/hook";
+import { setRoomId, setStartDate, setEndDate, setPeople } from "../store/slices/reserveSlice";
 
 const RoomDetail = () => {
   const { roomId } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const [roomData, setRoomData] = useState<Room>();
 
   //取得房間資料
   const getRoomData = async () => {
+
+    if (!roomId) {
+      navigate("/", { replace: true });
+      return;
+    } 
+
     const res = await apiGetRoom(roomId);
     if (res) {
       setRoomData(res.result);
@@ -42,17 +56,12 @@ const RoomDetail = () => {
     "為了確保所有客人的安全，請勿在走廊或公共區域大聲喧嘩，並遵守酒店的其他規定."
   ];
 
-  // const [selected, setSelected] = useState<Date>();
-
-  // let footer = <p>Please pick a day.</p>;
-  // if (selected) {
-  //   footer = <p>You picked {format(selected, 'PP')}.</p>;
-  // }
-
   const [defaultDate, setDefaultDate] = useState('');
   const [defaultCheckoutDate, setDefaultCheckoutDate] = useState('');
 
   useEffect(() => {
+    document.documentElement.scrollTop = 0;
+
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
 
@@ -63,34 +72,104 @@ const RoomDetail = () => {
     const day = currentDate.getDate().toString().padStart(2, '0');
 
     // 格式化日期字符串
-    const formattedDate = `${year}-${month}-${day}`;
-    const formattedTomorrow = `${tomorrow.getFullYear()}-${(tomorrow.getMonth() + 1).toString().padStart(2, '0')}-${tomorrow.getDate().toString().padStart(2, '0')}`;
+    const formattedDate = `${year}/${month}/${day}`;
+    const formattedTomorrow = `${tomorrow.getFullYear()}/${(tomorrow.getMonth() + 1).toString().padStart(2, '0')}/${tomorrow.getDate().toString().padStart(2, '0')}`;
 
     // 設置為 input 的預設值
     setDefaultDate(formattedDate);
     setDefaultCheckoutDate(formattedTomorrow);
   }, []);
 
-  //
-  const [selectedDate, setSelectedDate] = useState<Date | Date[]>(new Date());
-  const [showCalendar, setShowCalendar] = useState(false);
+  const [isSelectDateOpen, setIsSelectDateOpen] = useState(false); //日期選擇器是否開啟
+  const dateRangeRef = useRef<HTMLDivElement | null>(null);
+  //日期選擇器
+  const [selectDate, setSelectDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+      key: "selection",
+    },
+  ]);
+  const [roomNights, setRoomNights] = useState<number>(1); //訂房天數
+  const [totalPrice, setTotalPrice] = useState<number>(0); //總價格
 
-  const handleInputClick = () => {
-    setShowCalendar(true);
+  useEffect(() => {
+    setRoomNights(
+      calculateNights(selectDate[0].startDate, selectDate[0].endDate) !== 0
+        ? calculateNights(selectDate[0].startDate, selectDate[0].endDate)
+        : 1,
+    );
+  }, [selectDate]);
+
+  //計算總價格
+  useEffect(() => {
+    if (roomData) {
+      const price = roomData.price * roomNights;
+      setTotalPrice(price);
+    }
+  }, [roomNights, roomData]);
+
+  //計算訂房天數
+  const calculateNights = (startDate: Date, endDate: Date) => {
+    const oneDay = 24 * 60 * 60 * 1000; // 每天的毫秒數
+    const diffTime = Math.abs(endDate.getTime() - startDate.getTime());
+    const diffDays = Math.floor(diffTime / oneDay);
+    return diffDays;
   };
 
-  const handleCalendarClose = () => {
-    setShowCalendar(false);
+  //用於顯示訂房日期
+  const showFormattedDate = (date: Date) => {
+    return date.toLocaleDateString("zh-TW", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+    });
+  };
+
+  const handleInputClick = () => {
+    if (isSelectDateOpen) {
+      setIsSelectDateOpen(false);
+      setDefaultDate(showFormattedDate(selectDate[0].startDate));
+      setDefaultCheckoutDate(showFormattedDate(selectDate[0].endDate));
+    } else {
+      setIsSelectDateOpen(true);
+    }
   };
 
   const [count, setCount] = useState(2);
 
   const handleIncrement = () => {
-    setCount(count + 1);
+    if (!roomData) {
+      alert("房間資料有誤，請洽客服");
+      return;
+    }
+
+    const MAX_PEOPLE = roomData.maxPeople;
+    setCount(count + 1 > MAX_PEOPLE ? MAX_PEOPLE : count + 1);
   };
 
   const handleDecrement = () => {
     setCount(count > 1 ? count - 1 : 1);
+  };
+
+  const checkReserveInfo = () => {
+    if (!roomId) {
+      navigate("/", { replace: true });
+      return;
+    }
+
+    dispatch(setRoomId(roomId));
+    dispatch(setStartDate(showFormattedDate(selectDate[0].startDate)));
+    dispatch(setEndDate(showFormattedDate(selectDate[0].endDate)));
+    dispatch(setPeople(count));
+
+    setTimeout(() => {
+      navigate(`/reserve`, { replace: true });
+    }, 1000);
+  };
+
+  const goToRooms = () => {
+    navigate("/room");
   };
 
   return (
@@ -133,6 +212,7 @@ const RoomDetail = () => {
                   <Button
                     text="看更多"
                     btnType="secondary"
+                    onClick={goToRooms}
                   />
                 </div>
               </div>
@@ -221,11 +301,38 @@ const RoomDetail = () => {
               <div className="bg-white px-3 py-3 border rounded-3 border-primary border-opacity-25 d-flex">
                 <div className="py-3">
                   <p className="fs-24 pb-7">預訂房型</p>
-                  <h2 className="fw-bold">尊爵雙人房</h2>
-                  <p className="pb-6">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
-                  <div className="d-flex mb-7">
+                  <h2 className="fw-bold">{roomData?.name}</h2>
+                  <p className="pb-6">{roomData?.description}</p>
+                  <div className="position-relative d-flex mb-7">
                     <input className="w-50 form-control me-2" type="text" placeholder="入住" defaultValue={defaultDate} onClick={handleInputClick} />
                     <input className="w-50 form-control" type="text" placeholder="退房" defaultValue={defaultCheckoutDate} onClick={handleInputClick} />
+                    {isSelectDateOpen && (
+                      <div
+                        ref={dateRangeRef}
+                        style={{ position: "absolute", top: "60px", left: "0px" }}
+                      >
+                        <DateRange
+                          editableDateInputs={true}
+                          onChange={(item) =>
+                            setSelectDate([
+                              {
+                                startDate: item.selection.startDate || new Date(),
+                                endDate:
+                                  item.selection.endDate ||
+                                  new Date(
+                                    new Date().getTime() + 24 * 60 * 60 * 1000,
+                                  ),
+                                key: "selection",
+                              },
+                            ])
+                          }
+                          moveRangeOnFirstSelection={false}
+                          ranges={selectDate}
+                          locale={zhTW}
+                          minDate={new Date()}
+                        />
+                      </div>
+                    )}
                   </div>
 
                   <div className="row">
@@ -240,16 +347,13 @@ const RoomDetail = () => {
                       </button>
                     </div>
                   </div>
-                  <p className="m-0 fs-24 py-7 text-primary">NT$ 10,000</p>
+                  <p className="m-0 fs-24 py-7 text-primary">NT$ {totalPrice.toLocaleString("en-US")}</p>
                   <Button
                     text="立即預訂"
                     btnType="primary"
                     fit="container"
-                    onClick={handleCalendarClose}
+                    onClick={checkReserveInfo}
                   />
-                  {showCalendar && (
-                    <Calendar onChange={setSelectedDate} defaultValue={selectedDate} />
-                  )}
                 </div>
               </div>
 
